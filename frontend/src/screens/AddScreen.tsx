d4a7fd4db28ee4833298d78666910a3d2e2891d3
Mariia.Fadeeva <mariia.fadeeva@jetbrains.com>
2026-02-18 17:52:11 +0200

    Initialize Expo frontend project with basic setup files and dependencies Added main blank tabs Started implementing map tab

---
+++ frontend/src/screens/AddScreen.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+
+export default function AddScreen() {
+    return <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }} />;
+}
