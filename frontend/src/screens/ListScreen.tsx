3e74114a3a74050eeb0cfb078708927f151ed549
aleksandra.valerianova <sasha.val2006@gmail.com>
2026-02-25 13:02:07 +0200

    1st version of List screen

--- frontend/src/screens/ListScreen.tsx
+++ frontend/src/screens/ListScreen.tsx
@@ -1,6 +1,92 @@
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
+
+function ActionButton({ title }: { title: string }) {
+  return (
+    <TouchableOpacity style={s.button}>
+      <Text style={s.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
 
 export default function ListScreen() {
-    return <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }} />;
+    const navigation = useNavigation<any>();
+  return (
+//     <ImageBackground
+//       source={require("../../assets/background2.jpg")}
+//       style={s.background}
+//       resizeMode="cover"
+//     >
+      <SafeAreaView style={s.safe}>
+        <View style={s.container}>
+          <View style={s.row}>
+          <TouchableOpacity style={s.button} onPress={() => navigation.navigate("Find")}>
+                    <Text style={s.buttonText}>Find</Text>
+          </TouchableOpacity>
+            <View style={s.spacer} />
+            <ActionButton title="My Events" />
+          </View>
+
+          <View style={s.rowMarginTop} />
+
+          <View style={s.row}>
+            <ActionButton title="Favourites" />
+            <View style={s.spacer} />
+            <ActionButton title="History" />
+          </View>
+        </View>
+      </SafeAreaView>
+//     </ImageBackground>
+  );
 }
+
+const s = StyleSheet.create({
+//     background: { flex: 1 }, // ✅ иначе фон не виден
+  safe: {
+      flex: 1,
+      backgroundColor: "#fff",
+    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+
+  row: {
+    flexDirection: "row",
+    alignItems: "stretch",
+  },
+
+  spacer: { width: 20 }, // ✅ вместо gap
+
+  rowMarginTop: { height: 20 }, // ✅ вместо gap по вертикали
+
+  button: {
+    flex: 1,
+    height: 120,
+    backgroundColor: "rgba(255,255,255, 0.85)", // читабельнее на фоне
+    borderRadius: 24,
+    justifyContent: "center",
+    alignItems: "center",
+
+    shadowColor: "#000",
+    shadowOpacity: 0.15,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 4 },
+    elevation: 5,
+  },
+
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "800",
+    color: "#222",
+  },
+});
